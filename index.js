9651579e1f8e8a9ffee11bcc158662a809870cb5
Jayrajbhamre <jayrajbhamre@gmail.com>
2022-10-04 15:32:56 +0530

    all in one

---
+++ index.js
@@ -0,0 +1,45 @@
+//Arithmetic Operations
+
+
+let a= 100;
+let b= 20;
+let c=a+b;
+let S=a-b;
+let M=a*b;
+let D=a/b;
+let Mod=a%b;
+
+
+console.log(c,S,M,D,Mod);
+
+
+
+
+
+//comparision
+
+let g=8;
+let l=9;
+let total=g+l;
+let R=g>l;
+let R2=g<l;
+let R3= ++g;
+let R4= l++;
+let R5= (a==b);
+let R6= a===b;
+var R7=a!=b;
+var R7=a>b;
+const R8=a<b;
+const R9=a>=b;
+var R10=""+total;
+var R11="2"+4+4;
+let R12=a+=b;
+let R13=a-=b
+var R14=a*=b;
+var R15=a/=b;
+var R16=a%=b;
+
+console.log(R,total,R2,R3,R4.R5,R6,R7.R8,R9,R10,R11,R12,R13,R14,R15,R16);
+
+
+
